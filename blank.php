9f58089107b41a232c836c6391c7c3f5edb9909f
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2018-12-28 22:18:18 +0530

    Added ajax form

--- blank.php
+++ blank.php
@@ -16,16 +16,34 @@
 <body>
     
     <form action="https://stackoverflow.com/oauth/access_token" method="POST">
-        <input type="text" name="client_id" value="14150">
-        <input type="text" name="client_secret" value="gB2HO2LZdAjRrwfKH)7B*A((">
-        <input type="text" name="code" value="<?php echo $code; ?>">
-        <input type="text" name="redirect_uri" value="https://sl-mcq.herokuapp.com/blank.php">
-        <button type="submit">CLick me please</button>
+        <!-- <input type="text" name="client_id" value="14150"> -->
+        <!-- <input type="text" name="client_secret" value="gB2HO2LZdAjRrwfKH)7B*A(("> -->
+        <input type="text" id="code" name="code" value="<?php echo $code; ?>">
+        <!-- <input type="text" name="redirect_uri" value="https://sl-mcq.herokuapp.com/blank.php"> -->
+        <!-- <button type="submit">CLick me please</button> -->
     </form>       
 
     <script
     src="https://code.jquery.com/jquery-3.3.1.min.js"
     integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
     crossorigin="anonymous"></script>
+
+    <script>
+        var code = $('#code').attr('value');
+        $.ajax({
+            type: "POST",
+            url: "https://stackoverflow.com/oauth/access_token",
+            contentType="application/x-www-form-urlencoded",
+            data: {
+                client_id: "14150",
+                client_secret: "gB2HO2LZdAjRrwfKH)7B*A((",
+                code: code,
+                redirect_uri: "https://sl-mcq.herokuapp.com/blank.php"
+            },
+            success: function(response) {
+                alert(response);
+            }
+        });
+    </script>
 </body>
 </html>
