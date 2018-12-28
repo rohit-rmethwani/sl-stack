61e4a85c4f4be5d3bfed38d727c666260aeb3108
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2018-12-28 21:30:29 +0530

    Added JQUERY

--- test.php
+++ test.php
@@ -9,6 +9,10 @@
 <body>
     
     <script type='text/javascript' src='https://api.stackexchange.com/js/2.0/all.js'></script>
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
 
     <script type='text/javascript'>
         $.ajax({
