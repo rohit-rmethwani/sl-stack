bef5d1a310833dc3933f37af16cdaa542869be25
KhushbooChandnani <37927996+KhushbooChandnani@users.noreply.github.com>
2018-12-28 21:06:03 +0530

    Adding Base Files

---
+++ index.php
@@ -0,0 +1,154 @@
+<html>
+<head>
+   <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+   <link rel="stylesheet" href="vendors/font-awesome-4.7.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
+   <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="vendors/animate.css">
+
+    <link rel="stylesheet" href="vendors/magnific-popup.css">
+    
+    <link rel="stylesheet" href="vendors/assets/owl.carousel.min.css">
+    
+    <link rel="stylesheet" href="vendors/assets/owl.theme.default.css">
+    <link rel="stylesheet" href="css/style.css">
+  <!-- CSS Files -->
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg bg-info">
+            <div class="container">
+              <div class="navbar-translate">
+                <a class="navbar-brand" href="#0"><img src="img/studylink_logo.jpg" height="43px" style="margin-left:-100px;"
+                  ></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="navbar-toggler-icon"></span>
+                  <span class="navbar-toggler-icon"></span>
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+             </div>
+              <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+                  
+                  <li class="nav-item">
+                    <div class="col-sm-10">
+      <input type="text" id="inputSuccess" placeholder="Serch your query here ............">
+      
+    </div>
+                  </li>
+                  <li class="nav-item">
+                    <div class="col-sm-10 logg">
+                    <button class="btn btn-round log">LOG IN</button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+      
+    </div>
+                
+              
+            
+          </nav>
+
+<div class="col-md-2 sidebar"></div>
+        <div class="section section-examples" style="margin-top:-30px;">
+        	<div class="col-md-8">
+          <div class="first" style="background:white;">
+      
+        
+            
+              <img src="icons/help-round-button.png" alt="Rounded Image" width="40px" height="40px" class="rounded ques-img">
+              <h2 class="top-heading">Parse error : syntax error, unexcepted "", expecting identifier (T_STRING) or variable (T_VARIABLE) or number (T_NUM_STRING) [duplicate] parsing done completely (T_STRING) or variable.</h2>
+              
+              <textarea class="top-contain inner-contain" cols="100" rows="25"></textarea>
+              <div class="main-inner-contain">
+              <span><button class="btn html">HTML</button>
+              <button class="btn php">PHP</button>
+              <button class="btn wc3">WC3</button>
+              <button class="btn scripts">SCRIPTS</button></span><br>
+              <button class="btn btn-round by-person">By Khushboo Chandnani on 21-04-2018</button>
+            </div>
+
+             
+          </div>
+      <div class="">
+      	<h2 class="answers">4 Answers</h2>
+      </div>
+
+      <div class="middle" style="background:white;margin-top:30px;">
+       <img src="icons/support.png" alt="Rounded Image" width="45px" height="45px" class="rounded ques-img"><br>
+       <div class="row second-inner">
+              <div class="col-md-1" height=300px>
+                <input type="image" src="icons/sort-up.png" class="btTxt submit" style="padding:0px 0px 0px 30px" />
+                <p style="padding:10px 0px 0px 40px;font-size:20px;">3<p>
+                <input type="image" src="icons/sort-down.png" class="btTxt submit" style="padding:0px 0px 0px 30px" />
+              </div>
+
+              
+              <div class="col-md-8">
+                <textarea cols="100" rows="5" class="ques-answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </textarea>
+              <textarea class="ques-answer second-text" cols="100" rows="12"></textarea>
+              <textarea cols="100" rows="5" class="ques-answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ullamco laboris nisi ut aliquip ex ea commodo consequat
+              </textarea><br>
+              <span>
+                <div class="second-inner-contain">
+                <span><button class="btn by-person">SHARE</button>
+                <button class="btn by-person">IMPROVE</button>
+                <input type="text" id="inputSuccess" placeholder="ADD A COMMENT"></span><br>
+                <button class="btn btn-round by-person">By Khushboo Chandnani on 21-04-2018</button>
+                </div>
+            
+
+            </div>
+              </div>
+            </div>
+            <div class="middle" style="background:white;margin-top:30px;">
+       <img src="icons/support.png" alt="Rounded Image" width="45px" height="45px" class="rounded ques-img"><br>
+       <div class="row second-inner">
+              <div class="col-md-1" height=300px>
+                <input type="image" src="icons/sort-up.png" class="btTxt submit" style="padding:0px 0px 0px 30px" />
+                <p style="padding:10px 0px 0px 40px;font-size:20px;">3<p>
+                <input type="image" src="icons/sort-down.png" class="btTxt submit" style="padding:0px 0px 0px 30px" />
+                <input type="image" src="icons/correct.png" class="btTxt submit" style="margin:20px 0px 0px 25px" height="40px" width="40px"/>
+              </div>
+
+              
+              <div class="col-md-8">
+                <textarea cols="100" rows="5" class="ques-answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </textarea>
+              <textarea class="ques-answer second-text" cols="100" rows="12"></textarea>
+              <textarea cols="100" rows="5" class="ques-answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ullamco laboris nisi ut aliquip ex ea commodo consequat
+              </textarea><br>
+              <span>
+                <div class="second-inner-contain">
+                <span><button class="btn by-person">SHARE</button>
+                <button class="btn by-person">IMPROVE</button>
+                <input type="text" id="inputSuccess" placeholder="ADD A COMMENT"></span><br>
+                <button class="btn btn-round by-person">By Khushboo Chandnani on 21-04-2018</button>
+                </div>
+            
+
+            </div>
+              </div>
+            </div>
+            
+        </div>
+       <div class="col-md-2 sidebar"></div>
+        
+    
+        <script src="vendors/jquery/jquery-3.3.1.min.js"></script>
+        <!--Bootstrap Script-->
+        <script src="vendors/bootstrap/js/bootstrap.min.js"></script>
+        <!--END OF CORE JS-->
+        <!--Custom Js-->
+        <script src="vendors/WOW-master/dist/wow.min.js"></script>
+        <script src="vendors/jquery.magnific-popup.min.js"></script>
+        <script src="vendors/owl.carousel.min.js"></script>
+        <script src="vendors/jquery.waypoints.min.js"></script>
+        <script src="vendors/jquery.counterup.min.js"></script>
+</body>
+</html>
